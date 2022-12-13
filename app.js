6236ee8263decdcaf66107a0ff184d6975f3f033
JamissonBarbosa <jamissondasilvafilho@gmail.com>
2022-12-13 10:59:49 -0300

    projeto iniciado

---
+++ app.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.status(200).json({msg: "Hello World"})
+})
+app.listen(3000)
